1884383183f3857977531acaa0b48bd886342e28
Knowledge Shelf <60973040+knowledgeshelfit@users.noreply.github.com>
2021-09-28 12:42:04 +0530

    Add files via upload

---
+++ Day-1/main_mod/sub_mod/mod2.py
@@ -0,0 +1,9 @@
+def func3():
+    num1 = int(input("num1:"))
+    num2 = int(input("num2:"))
+    num3 = pow(num1,num2)
+    return num3
+
+def func4(num1,num2):
+    num3 = num1/num2
+    return num3

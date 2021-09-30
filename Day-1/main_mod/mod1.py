65a1e3a5a8fe1226c3bcd3a3b4266ba120031c9a
Knowledge Shelf <60973040+knowledgeshelfit@users.noreply.github.com>
2021-09-30 12:02:17 +0530

    Delete Day-1 directory

--- Day-1/main_mod/mod1.py
+++
@@ -1,9 +0,0 @@
-def tabel():
-    num = int(input("Enter the value:"))
-    for i in range(1,11):
-        num2 = i * num
-        print(f"{num} * {i} = {num2}")
-        
-def func2(var1,var2):
-    var3 = var1 + var2
-    print("Sum:",var3)
